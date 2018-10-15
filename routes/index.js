0b16ae21a01e0723d3da67675c726dfed1f391a8
Fabienne <fabienne.lienhard@hsr.ch>
2018-10-15 11:01:12 +0200

    change theme step 1

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+document.getElementById('colorie').onclick = function () {
+    document.getElementById('other_theme').href = '../red.css';
+};
+
 module.exports = router;
