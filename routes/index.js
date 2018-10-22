ef02dfb58c0235719ee8df27dfb6277c570eb299
Fabienne <fabienne.lienhard@hsr.ch>
2018-10-22 09:23:59 +0200

    change theme where it actually works ^^

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', theme: '/stylesheets/style.css', theOtherSiteThemeWise: '/otherTheme'});
+});
+
+/* Change the theme, at least in theorie*/
+router.get('/otherTheme', function(req, res, next) {
+  res.render('index', {title: 'stillExpress', theme: '/stylesheets/pink.css', theOtherSiteThemeWise: '/'});
 });
 
 
-//document.getElementById('colorie').onclick = function () {
-//    document.getElementById('theme').href = '/stylesheets/pink.css';
-//};
 
 module.exports = router;
+
+
